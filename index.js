d88a96ed2cc088b22ffe27691f2f78031e470a3e
Shadab Sheikh <9545shekh@gmail.com>
2025-08-29 14:55:04 +0530

    upadated index.js

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ app.use("/", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("🚀 UrbanMedex Backend is running successfully!");
+});
+
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
